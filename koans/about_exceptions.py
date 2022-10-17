096af382485cecf8f7f1ef69e8dd3bdd7d111489
Corey Hawthorne <chawthorne02@gmail.com>
2022-10-17 15:08:57 -0400

    completed some koans lessons with notes

--- koans/about_exceptions.py
+++ koans/about_exceptions.py
@@ -5,15 +5,22 @@ from runner.koan import *
 
 class AboutExceptions(Koan):
 
+
+    #https://docs.python.org/3/library/exceptions.html?highlight=runtime%20error#RuntimeError
+    # Raised when an error is detected that doesn’t fall in any of the other categories. The associated value is a string indicating what precisely went wrong.
+    # Exception is an error that happens during the execution of a program. Whenever there is an error, Python generates an exception that could be handled. It basically prevents the program from getting crashed.
+    # Exceptions must be instances of a class that derives from BaseException. In a try statement with an except clause that mentions a particular class, that clause also handles any exception classes derived from that class
+    # Objects is a base for all classes. It has methods that are common to all instances of Python classes. This function does not accept any arguments.
+
     class MySpecialError(RuntimeError):
         pass
 
     def test_exceptions_inherit_from_exception(self):
         mro = self.MySpecialError.mro()
-        self.assertEqual(__, mro[1].__name__)
-        self.assertEqual(__, mro[2].__name__)
-        self.assertEqual(__, mro[3].__name__)
-        self.assertEqual(__, mro[4].__name__)
+        self.assertEqual("RuntimeError", mro[1].__name__)
+        self.assertEqual("Exception", mro[2].__name__)
+        self.assertEqual("BaseException", mro[3].__name__)
+        self.assertEqual("object", mro[4].__name__)
 
     def test_try_clause(self):
         result = None
@@ -24,15 +31,15 @@ class AboutExceptions(Koan):
 
             ex2 = ex
 
-        self.assertEqual(__, result)
+        self.assertEqual("exception handled", result)
 
-        self.assertEqual(__, isinstance(ex2, Exception))
-        self.assertEqual(__, isinstance(ex2, RuntimeError))
+        self.assertEqual(True, isinstance(ex2, Exception))
+        self.assertEqual(False, isinstance(ex2, RuntimeError))
 
         self.assertTrue(issubclass(RuntimeError, Exception), \
             "RuntimeError is a subclass of Exception")
 
-        self.assertEqual(__, ex2.args[0])
+        self.assertEqual("Oops", ex2.args[0])
 
     def test_raising_a_specific_error(self):
         result = None
@@ -42,8 +49,8 @@ class AboutExceptions(Koan):
             result = 'exception handled'
             msg = ex.args[0]
 
-        self.assertEqual(__, result)
-        self.assertEqual(__, msg)
+        self.assertEqual("exception handled", result)
+        self.assertEqual("My Message", msg)
 
     def test_else_clause(self):
         result = None
@@ -55,7 +62,7 @@ class AboutExceptions(Koan):
         else:
             result = 'no damage done'
 
-        self.assertEqual(__, result)
+        self.assertEqual('no damage done', result)
 
 
     def test_finally_clause(self):
@@ -68,4 +75,4 @@ class AboutExceptions(Koan):
         finally:
             result = 'always run'
 
-        self.assertEqual(__, result)
+        self.assertEqual("always run", result)
